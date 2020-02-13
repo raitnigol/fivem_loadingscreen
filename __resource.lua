be23adaa00b44047e030bca18e2c21e05d1d867f
Nigol <rait.nigol@khk.ee>
2020-02-13 16:58:15 +0200

    Add files via upload

--- __resource.lua
+++ __resource.lua
@@ -7,7 +7,6 @@ files {
     'js/demo.js',
     'js/jquery.particleground.js',
     'js/config.js',
-    'js/music-controls.js',
     'js/music-handler.js',
     'js/showRealTime.js',
     'js/synn.js',
@@ -16,5 +15,4 @@ files {
 }
 
 loadscreen 'index.html'
-
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
